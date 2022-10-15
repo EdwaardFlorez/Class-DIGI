ca81c7d410467eaa53dbe9160cdee5828633b739
Edward Flórez <edwardflorez1@hotmail.com>
2022-10-14 23:50:02 -0500

    tailwind initial configuration

---
+++ tailwind.config.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      blue: "#dceaf7",
+      purple: "#2c2a50",
+      "purple-light": "#545382",
+      pink: "#ff49db",
+      red: "#fb5760",
+      green: "#aab0d8",
+      yellow: "#ffc82c",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
+};
